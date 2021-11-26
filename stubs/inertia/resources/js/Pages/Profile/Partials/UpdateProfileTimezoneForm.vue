05eb895195ea6ba8b0acdd7f10f744338a3549c6
Sven Roelse <acvxqs@icloud.com>
2021-11-26 19:05:03 +0100

    Fix fix fix

--- stubs/inertia/resources/js/Pages/Profile/Partials/UpdateProfileTimezoneForm.vue
+++ stubs/inertia/resources/js/Pages/Profile/Partials/UpdateProfileTimezoneForm.vue
@@ -14,7 +14,7 @@
                 <jet-label for="timezone" value="Timezone" />
                 <select id="timezone" class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm" @input="$emit('update:modelValue', $event.target.value)" ref="input">
                     <option class="display:none"></option>
-                    <option v-for="timezone in timeZonesList" :key="timezone" :value="timezone" :selected="timezone == modelValue">
+                    <option v-for="timezone in timeZonesList" v-model="timezone" :key="timezone" :value="timezone" :selected="timezone == modelValue">
                         {{ timezone }}
                     </option>
                 </select>
